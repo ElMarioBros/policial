00fc73b4c1da5bdb1ab2ee6dbb59b78c2a7481c1
dev <dev@relred.com>
2022-05-13 02:28:12 -0700

    ruta register eliminada

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <!--
 @ extends('adminlte::auth.register') 
-    <html>
-        <meta http-equiv="refresh" content="0; url=https://raut.cc">
-    </html>
 -->
+<html>
+    <meta http-equiv="refresh" content="0; url=https://raut.cc">
+</html>
